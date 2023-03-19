0788354fec70876dde8d2091c1931b0515496060
Marcinn88 <111724779+Marcinn88@users.noreply.github.com>
2023-03-19 12:37:44 +0100

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,26 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const gallery = document.querySelector('.gallery');
+
+//Creating and rendering tags for gallery items
+const galleryItem = galleryItems
+  .map(
+    item => `
+    <a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a>`
+  )
+  .join('');
+gallery.insertAdjacentHTML('beforeend', galleryItem);
+
+//Initialization of library SimpleLightBox
+const lightbox = new SimpleLightbox('.gallery a', {
+  caption: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
