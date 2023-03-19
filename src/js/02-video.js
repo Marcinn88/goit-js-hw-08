7ea40c1b1a6f12e65723c470f5a149c8217eb62d
Marcinn88 <111724779+Marcinn88@users.noreply.github.com>
2023-03-19 13:49:00 +0100

    form

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+//Adding lodash throttle from library
+import { throttle } from 'lodash';
 
+//Initializing pre-existing player
+import Player from '@vimeo/player';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+// Adding key to storage
+const STORAGE_KEY = 'videoplayer-current-time';
+const savedTime = localStorage.getItem(STORAGE_KEY);
+
+//Saving timeupdate in storage with key
+function savePlayedTime(data) {
+  localStorage.setItem(STORAGE_KEY, data.seconds);
+}
+
+//Using lodash.throttle to update storage once for one secound and tracking timeupdate with on() metod
+player.on('timeupdate', throttle(savePlayedTime, 1000));
+
+//Using setCurrentTime() metod to reload page and play video from saved time
+player.setCurrentTime(savedTime);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+import { throttle } from 'lodash';
+const STORAGE_KEY = `feedback-form-state`;
 
+const form = document.querySelector(`.feedback-form`);
+const email = document.querySelector(`[name="email"]`);
+const message = document.querySelector(`[name="message"]`);
+const savedData = {};
+
+afterReload();
+
+form.addEventListener('input', throttle(handleInput, 500));
+form.addEventListener('submit', handleSubmit);
+
+function handleInput(event) {
+  savedData[event.target.name] = event.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(savedData));
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const praseData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  if (praseData) {
+    console.log(praseData);
+  }
+  event.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function afterReload() {
+  const praseData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  if (praseData) {
+    email.value = praseData.email;
+    message.value = praseData.message;
+  } else {
+    email.value = '';
+    message.value = '';
+  }
+}
