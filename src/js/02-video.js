3cc6752cede8bb2b7e6cf3904cad6d87862dbe4b
Marcinn88 <111724779+Marcinn88@users.noreply.github.com>
2023-03-21 21:59:42 +0100

    fix

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -7,17 +7,16 @@ import Player from '@vimeo/player';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-// Adding key to storage
 const STORAGE_KEY = 'videoplayer-current-time';
 const savedTime = localStorage.getItem(STORAGE_KEY);
 
-//Saving timeupdate in storage with key
 function savePlayedTime(data) {
   localStorage.setItem(STORAGE_KEY, data.seconds);
 }
 
-//Using lodash.throttle to update storage once for one secound and tracking timeupdate with on() metod
 player.on('timeupdate', throttle(savePlayedTime, 1000));
 
-//Using setCurrentTime() metod to reload page and play video from saved time
-player.setCurrentTime(savedTime);
+const timeSum = localStorage.getItem(STORAGE_KEY);
+if (timeSum !== null) {
+  player.setCurrentTime(savedTime);
+}
